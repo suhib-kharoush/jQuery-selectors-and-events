e9f017ac941c5c741faa877db1bebb34891bdc67
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-03-27 10:51:50 +0300

    almost finish

--- js/app.js
+++ js/app.js
@@ -1,88 +1,124 @@
 "use strict";
 
-let arrayKeyword = [];
-let userOption = [];
-let uniqueOptionKey = [];
-
-
-function GalleryHorn(horn) {
-    this.image_url = horn.image_url;
-    this.title = horn.title;
-    this.description = horn.description;
-    this.keyword = horn.keyword;
-    this.horns = horn.horns
-    arrayKeyword.push(this);
-}
-
-
-// function GalleryHorn2(horn2) {
-//     this.image_url = horn2.image_url;
-//     this.title = horn2.title;
-//     this.description = horn2.description;
-//     this.keyword = horn2.keyword;
-//     this.horns = horn2.horns;
-//     SecondArrayKeyword2.push(this);
-// }
-
-
-// GalleryHorn.prototype.cloneRender = function() {
-//     let cloneSection = $('.photo-template').clone();
-//     cloneSection.find('h2').text(this.title);
-//     cloneSection.find('img').attr('src', this.image_url);
-//     cloneSection.find('p').text(this.description);
-//     cloneSection.removeClass('photo-template');
-//     cloneSection.attr('class', this.title);
-//     $('main').append(cloneSection);
-// }
-
-GalleryHorn.prototype.renderWithMustache = function() {
-    let template = $("#template").html();
-    $("main").append(Mustache.render(template, this))
-        // let html = Mustache.render(template, this);
-        // $("#photo-template").append(html);
-
-}
-
-
-
-
-// GalleryHorn2.prototype.renderWithMustache2 = function() {
-//     let template = $("#template").html();
-//     $("#secondPage").append(Mustache.render(template, this));
-//     let html = Mustache.render(template, this);
-//     $(".photo-template").append(html);
-
-// }
-
-const ajaxSettings = {
-    method: 'get',
-    dataType: 'json',
-};
-
-$.ajax('data/page-1.json', ajaxSettings).then((data) => {
-    data.forEach((horn) => {
-        let hornObject = new GalleryHorn(horn);
-        hornObject.renderWithMustache();
-        userOption.push(horn.keyword);
-    });
-
-    $.each(userOption, function(i, value) {
-        if ($.inArray(value, uniqueOptionKey) === -1) uniqueOptionKey.push(value);
-    });
-    uniqueOptionKey.forEach(function(value, i) {
-        $('#sortOptions').append(`<option value ="${value}"> ${value} </option>`)
-
-        $('#sortOptions').on('change', function() {
-            let keyName = this.options[this.selectedIndex].text;
-            let newCloneSection = $('.photo-template').clone();
-            $('main').html("");
-            $('main').append(newCloneSection);
-            arrayKeyword.forEach((function(value) {
-                if (keyName === value.keyword) {
-                    value.renderWithMustache();
-
-                }
-            }));
-        });
-    });
+$(document).ready(function() {
+    getFromPage(1)
 })
+
+
+Horn.all = [];
+
+function Horn(item) {
+    this.image_url = item.image_url;
+    this.title = item.title;
+    this.description = item.description;
+    this.keyword = item.keyword;
+    this.horns = item.horns;
+}
+
+Horn.prototype.render = function() {
+    let template = $('#hornTemplate').html();
+    let html = Mustache.render(template, this);
+    // $('main').append(html);
+    let final = $('#photo-template').append(html);
+    $('main').append(final);
+}
+
+function getFromPage(page) {
+    $('#photo-template').empty();
+    // $('main').empty();
+    Horn.all = [];
+    $.get(`data/page-${page}.json`)
+        .then(data => {
+            data.forEach((val, idx) => {
+                let NewHorn = new Horn(val);
+                Horn.all.push(NewHorn);
+            })
+
+            Horn.all.forEach((val, idx) => {
+                val.render();
+            })
+            fillKeywords();
+            filterByKeyword();
+            sortHorns();
+        })
+}
+
+function fillKeywords() {
+    $('.filter').empty();
+    $('.filter').append(`<option value="default">Filter By Keyword</option>`);
+    let fillKeywordsList = [];
+    Horn.all.forEach((val, idx) => {
+        if (!fillKeywordsList.includes(val.keyword)) {
+            fillKeywordsList.push(val.keyword);
+        }
+    })
+
+    fillKeywordsList.forEach((val, idx) => {
+        let optionTag = `<option value="${val}">${val}</option>`;
+        $('.filter').append(optionTag);
+    })
+}
+
+let afterFilter = '';
+let selected = '';
+
+function filterByKeyword() {
+    afterFilter = '';
+    $('.filter').on('change', function() {
+        $('div').hide();
+        let selected = $(this).val();
+        if (selected === 'default') {
+            $('div').show();
+            $('div').eq(0).remove();
+        } else {
+            $(`div[id="${selected}"]`).fadeIn();
+            afterFilter = selected;
+        }
+
+
+    })
+}
+
+
+$('button').on('click', function() {
+    let buttonId = $(this).attr('id');
+    getFromPage(buttonId);
+})
+
+
+function sortHorns() {
+    $('.sort').on('change', function() {
+        if ($(this).val() === 'title') {
+            sortingAlgorithm(Horn.all, 'title');
+        } else if ($(this).val() === 'number') {
+            sortingAlgorithm(Horn.all, 'horns');
+        }
+    })
+}
+
+
+function sortingAlgorithm(array, property) {
+    array.sort((a, b) => {
+        let partA = a[property];
+        let partB = b[property];
+        if (partA > partB) return 1;
+        if (partA < partB) return -1;
+        else return 0;
+    })
+    $('#photo-template').html('');
+    if (selected === 'default' || afterFilter === '') {
+        Horn.all.forEach(val => {
+            val.render();
+        })
+    } else {
+        let newDataOfHorns = [];
+        Horn.all.forEach(val => {
+            if (val.keyword == afterFilter) {
+                newDataOfHorns.push(val);
+            }
+        })
+        newDataOfHorns.forEach(val => {
+            val.render();
+        })
+    }
+}
